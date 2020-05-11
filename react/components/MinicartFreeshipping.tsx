7e279b5ba7f99e65b53f94d607b5fdaabf441852
Saulo Martins <saulo.martins@acct.global>
2020-05-11 17:02:28 -0300

    custom minicart freeshipping component structure

---
+++ react/components/MinicartFreeshipping.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useCallback, useState } from 'react'
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+
+import styles from './MinicartFreightScale.css'
+
+interface Props {
+    minFreightValue: number
+}
+
+const MinicartFreightScale: StorefrontFC<Props> = ({ minFreightValue }) => {
+    const [shippingFreePercentage, setShippingFreePercentage] = useState(0)
+    const [differenceBetwenValues, setDifferenceBetwenValues] = useState(0)
+
+    const {
+        orderForm: { value },
+    } = useOrderForm()
+
+    const handleCloseMinicart = () => {
+        const minicartCloseButton: any = document.querySelector(
+            '.vtex-minicart-2-x-closeIconButton'
+        )
+
+        minicartCloseButton.click()
+    }
+
+    useEffect(() => {
+        handleUpdateMinicartValue(value)
+    }, [value])
+
+    const handleUpdateMinicartValue = useCallback(
+        value => {
+            setShippingFreePercentage(Math.round(value / minFreightValue))
+            setDifferenceBetwenValues(minFreightValue - value / 100)
+        },
+        [setShippingFreePercentage, setDifferenceBetwenValues]
+    )
+
+    return (
+        <div className={styles.freigthScaleContainer}>
+            <div className={styles.sliderContainer}>
+                <div
+                    style={{
+                        width: `${
+                            shippingFreePercentage < 100
+                                ? shippingFreePercentage
+                                : 100
+                        }%`,
+                        background: 'black',
+                        height: 10,
+                        transition: 'width 1s',
+                    }}
+                />
+            </div>
+            {differenceBetwenValues > 0 ? (
+                <p className={styles.sliderText}>
+                    Faltam{' '}
+                    <strong style={{ fontSize: '20px' }}>
+                        {Math.max(0, differenceBetwenValues).toLocaleString(
+                            'pt-br',
+                            {
+                                style: 'currency',
+                                currency: 'BRL',
+                            }
+                        )}
+                    </strong>{' '}
+                    para ganhar frete grátis
+                </p>
+            ) : (
+                <p className={styles.sliderText}>
+                    <strong>Você ganhou frete grátis</strong>
+                </p>
+            )}
+            <p className={styles.keepBuyingText} onClick={handleCloseMinicart}>
+                continuar comprando
+            </p>
+        </div>
+    )
+}
+
+MinicartFreightScale.defaultProps = {
+    minFreightValue: 500,
+}
+
+MinicartFreightScale.schema = {
+    title: 'Minicart Freight Scale',
+    description: 'Minicart Freight Scale',
+    type: 'object',
+    properties: {
+        minFreightValue: {
+            title: 'Valor mínimo para Frete Grátis',
+            type: 'number',
+        },
+    },
+}
+
+export default MinicartFreightScale
