acf7b0f573dace582151ca355be969528656a106
Saulo Martins <saulo.martins@acct.global>
2020-06-10 18:51:17 -0300

    buy buttons with custom texts finished

--- react/components/BuyButton.tsx
+++ react/components/BuyButton.tsx
@@ -1,15 +1,33 @@
 import { ExtensionPoint } from 'vtex.render-runtime'
 
 import React from 'react'
+import { injectIntl } from 'react-intl'
+
+import styles from './BuyButton.css'
+
 // import { Container } from './styles';
 
-const BuyButton = () => {
+const BuyButton = props => {
   return (
-    <div>
-      <ExtensionPoint id="rich-text" />
-      <ExtensionPoint id="add-to-cart-button" />
+    <div className={styles.buyButtonRow}>
+      <div className={styles.buyButton}>
+        <ExtensionPoint
+          id="add-to-cart-button"
+          text={props.intl.formatMessage({
+            id: 'store/buy-button',
+          })}
+        />
+      </div>
+      <div className={styles.addToCartButton}>
+        <ExtensionPoint
+          id="add-to-cart-button"
+          text={props.intl.formatMessage({
+            id: 'store/add-to-cart-button',
+          })}
+        />
+      </div>
     </div>
   )
 }
 
-export default BuyButton
+export default injectIntl(BuyButton)
